c7180f30d081061357da8d4c78d7b12b5cee7ab2
Chris Wong <hi.imhpc@outlook.com>
2018-10-13 17:39:23 +0800

    some res & login (login 的php我写了一点点，查表交给你们了）

---
+++ php/login.php
@@ -0,0 +1,13 @@
+<?php
+// edit by hpc
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if($username ==        )
+    {
+    echo json_encode();
+    }
+    else
+    {
+
+    }
