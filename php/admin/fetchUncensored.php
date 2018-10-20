d4d21aac729cb3941489584c4eb5d21e274157d4
Jim <1120845667@qq.com>
2018-10-20 22:41:45 +0800

    抓取未审核人员信息

--- php/admin/fetchUncensored.php
+++ php/admin/fetchUncensored.php
@@ -1,19 +1,6 @@
 <?php
 require("../database.php");
-$data=get("user","isCensored","0");
-$info=[];
-foreach($data as $_person){
-    $person=[];
-    $company=[];
-    $companyInfo=[];
-    array_push($person,$_person['username']);
-    array_push($person,$_person['name']);
-    array_push($person,$_person['phone']);
-    array_push($person,$_person['mail']);
-    $companyInfo = get("company","id",$_person['company']);
-    $company = $companyInfo[0]["companyName"];
-    array_push($person,$company);
-    array_push($info,$person);
-}
-echo json_encode($info);
+$sql="SELECT user.name,user.phone,user.mail,company.companyName,user.id FROM user INNER JOIN company ON user.company=company.id WHERE user.isCensored=0";
+$data=sql_str($sql);
+echo json_encode($data);
 ?>
