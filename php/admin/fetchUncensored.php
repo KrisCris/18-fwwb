3bbe27509ab07f938cb607b9f5013a6f40fc95b1
Jim <1120845667@qq.com>
2018-10-23 12:45:49 +0800

    改了获取数据的优先级

--- php/admin/fetchUncensored.php
+++ php/admin/fetchUncensored.php
@@ -1,6 +1,6 @@
 <?php
 require("../database.php");
-$sql="SELECT user.name,user.phone,user.mail,company.companyName,user.id FROM user INNER JOIN company ON user.company=company.id WHERE user.isCensored=0";
+$sql="SELECT user.name,user.phone,user.mail,company.companyName,user.id FROM user LEFT JOIN company ON user.company=company.id WHERE user.isCensored=0";
 $data=sql_str($sql);
 echo json_encode($data);
 ?>
