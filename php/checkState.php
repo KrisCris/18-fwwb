5e1739aa2da798725cb90b09449da1956e798800
Jim <1120845667@qq.com>
2018-10-23 20:10:09 +0800

    获取登录状态

---
+++ php/checkState.php
@@ -0,0 +1,19 @@
+<?php
+  header("Content-type:text/html;charset=utf-8");
+  require("database.php");
+  $uuid=$_POST["uuid"];
+  $group=$_POST["group"];
+  $code;
+
+  if($group==null){
+    $data=get("administrator","token",$uuid);
+  }
+  else{
+    $data=get("user","token",$uuid);
+  }
+
+  $code=empty($data)?0:1;
+
+  echo json_encode($code);
+
+?>
