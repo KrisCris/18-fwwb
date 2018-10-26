b7ab640d7eac48cc1d65a937410a16faa439a941
Chris Wong <hi.imhpc@outlook.com>
2018-10-26 14:27:08 +0800

    加了个修改信息的html

--- php/login/facialRecog.php
+++ php/login/facialRecog.php
@@ -3,6 +3,10 @@ require("../facialRecog.php");
 $facialData = $_POST['facialData'];
 $username = $_POST['username'];
 //去数据库里查询username的facialdata,存在dbFace里，用面部识别函数比对
+///
+///
+///
+///
 $dbFace = [];
 if(facialRecog($dbFace,$facialData))
 {
