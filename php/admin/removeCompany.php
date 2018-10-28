ab11608e750e9b44e7da13edd58ade082e598691
Jim <1120845667@qq.com>
2018-10-28 17:38:04 +0800

    更新

--- php/admin/removeCompany.php
+++ php/admin/removeCompany.php
@@ -3,7 +3,6 @@ header("Content-type:text/html;charset=utf-8");
 require("../database.php");
 $companyList=$_POST["companyList"];
 
-$companyList=explode(",",$companyList);           //输入字符串吧，“1,2,3,4” 我在php中通过逗号进行分割放入array数组中
 $allCompany=sql_str("SELECT id FROM company");
 $companyIdArray=array();
 foreach($allCompany as $each){
