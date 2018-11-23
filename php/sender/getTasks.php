0d43e34cb0c449150d396d8ea06230a89bbad063
Jim <1120845667@qq.com>
2018-11-23 19:14:58 +0800

    11.23

---
+++ php/sender/getTasks.php
@@ -0,0 +1,50 @@
+<?php  
+header("Content-type:text/html;charset=utf-8"); 
+date_default_timezone_set('Asia/Shanghai');  
+require("../taskStateCheck.php");  
+ 
+$uuid=$_POST["uuid"];  
+$group=$_POST["group"]; 
+$state=$_POST["state"]; 
+$code=0;  
+$taskArr=array(); 
+ 
+$user=get("user","token",$uuid);    
+if(!empty($user)){ 
+    $sql="SELECT id FROM project WHERE publisherId=".$user[0]['id']; 
+    $projectId=sql_str($sql); 
+    $nameArr=array(); 
+    $taskList=array(); 
+    foreach($projectId as $each){ 
+        $sql="SELECT task.taskName,task.startTime,task.endTime,task.securityLevel,task.state,project.prjName,task.userId FROM task INNER JOIN project WHERE task.prjId=project.id AND task.state=".$state." AND project.id=".$each['id']." ORDER BY task.endTime"; 
+        $tasks=sql_str($sql); 
+        if(!empty($tasks)){ 
+            $count=0; 
+            foreach($tasks as $each){ 
+                if($each["userId"]!=NULL){
+                    $sql="SELECT name FROM user WHERE id=".$each["userId"]; 
+                    $getName=sql_str($sql); 
+                    $tasks[$count]["receiver"]=$getName[0]["name"];
+                }
+
+                else{
+                    $tasks[$count]["receiver"]="";
+                }
+                $count++; 
+            } 
+            // array_push($taskArr,$tasks); 
+             
+            $taskArr = array_merge($taskArr,$tasks);  
+        } 
+    } 
+     
+    $code=1; 
+}  
+$json=array( 
+    "tasks"=>$taskArr, 
+    "code"=>$code 
+);    
+ 
+echo json_encode($json,JSON_UNESCAPED_UNICODE);  
+ // auther：hgz 
+?> 
---
+++ php/sender/taskPublish.php
@@ -0,0 +1,86 @@
+<?php  
+header("Content-type:text/html;charset=utf-8");  
+require("../database.php");  
+function Directory( $dir ){   
+    return  is_dir ( $dir ) or Directory(dirname( $dir )) and  mkdir ( $dir , 0777); 
+} 
+ 
+$uuid=$_POST["uuid"];  
+$skill=$_POST["skill"];  
+$newSkill=$_POST["newSkill"];  
+$description=$_POST["description"];  
+// $securityLevel=$_POST["securityLevel"];  
+$title=$_POST["title"];  
+$prjId=$_POST["prjId"]; 
+$startTime=$_POST["startTime"]; 
+$endTime=$_POST["endTime"]; 
+$imgname = $_FILES['file']['name'];                   //文件名 
+$tmp = $_FILES['file']['tmp_name'];                   //文件... 
+$data=array(); 
+$code=0; 
+$exist=0; 
+ 
+$user=get("user","token",$uuid);  
+$project=get("project","id",$prjId);                 //查找有无这个工程 
+if(empty($project)||empty($user)){ 
+    echo $json=json_encode(array(  
+        "code"=>$code, 
+    )); 
+    exit();                                         //没有结束脚本 
+} 
+else if(!empty($project)){ 
+    if($project[0]["publisherId"]!=$user[0]["id"]){     //检验任务id与工程是否匹配，不匹配结束脚本 
+        echo $json=json_encode(array(  
+            "code"=>$code, 
+        )); 
+        exit();   
+    } 
+    else{ 
+        $prjName=$project[0]["prjName"]; 
+        $sql="SELECT taskName FROM task WHERE prjId=$prjId"; 
+        $taskName=sql_str($sql);  
+        $taskList=array(); 
+        // echo $json=json_encode($taskName); 
+        foreach($taskName as $each){ 
+            array_push($taskList,$each["taskName"]); 
+        } 
+        if(in_array($title,$taskList)){ 
+            $code=-1;                                  //-1表示该文件名已存在 
+            echo $json=json_encode(array(  
+                "code"=>$code, 
+            )); 
+            exit();   
+        } 
+        else{ 
+            $pathFile="../../files/prj/".$prjName."/".$title."/"."sender/";   //多级目录 
+            $isbuilt=Directory($pathFile);              //创建多级目录 
+            if($isbuilt){ 
+                $workFile=$pathFile.$imgname;      //文件路径 
+                $skills=implode("*", $skill); 
+                if(!empty($newSkill)){ 
+                    foreach($newSkill as $each){ 
+                        $sql="ALTER TABLE skill ADD"." ".$each." "."varchar(255) NOT NULL"; 
+                        sql_str($sql);  
+                    } 
+                    $newSkills=implode("*", $newSkill); 
+                    $skills.="*".$newSkills; 
+                } 
+                $newtask=array(array("prjId",$prjId),array("startTime",$startTime),array("endTime",$endTime),array("taskName",$title),array("description",$description),array("workDescription",$workFile),array("text",$skills)); 
+                add("task",$newtask); 
+                $myfile = fopen($workFile, "w");     //创建多及目录文件                         2步骤 
+                if(move_uploaded_file($tmp,$workFile)){  //将传入文件导入该文件中 
+                    $code=1; 
+                }else{ 
+                } 
+            } 
+        } 
+    } 
+} 
+                   
+ 
+$data=array( 
+    "code"=>$code, 
+); 
+echo json_encode($data,JSON_UNESCAPED_UNICODE); 
+ // auther：hgz 
+?>
