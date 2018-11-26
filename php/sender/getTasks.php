29d013796149ca29606a87cb12426bf74cb85353
Jim <1120845667@qq.com>
2018-11-26 20:37:37 +0800

    修改2

--- php/sender/getTasks.php
+++ php/sender/getTasks.php
@@ -6,6 +6,7 @@ require("../taskStateCheck.php");
 $uuid=$_POST["uuid"];  
 $group=$_POST["group"]; 
 $state=$_POST["state"]; 
+$prjId=isset($_POST["prjId"])?$_POST["prjId"]:NULL;
 $code=0;  
 $taskArr=array(); 
  
@@ -15,16 +16,52 @@ if(!empty($user)){
     $projectId=sql_str($sql); 
     $nameArr=array(); 
     $taskList=array(); 
-    foreach($projectId as $each){ 
-        $sql="SELECT task.taskName,task.id,task.startTime,task.endTime,task.securityLevel,task.state,project.prjName,task.userId FROM task INNER JOIN project WHERE task.prjId=project.id AND task.state=".$state." AND project.id=".$each['id']." ORDER BY task.endTime"; 
+    if(empty($prjId)){
+        foreach($projectId as $each){ 
+            $sql="SELECT task.taskName,task.id,task.startTime,task.endTime,task.securityLevel,task.state,project.prjName,task.userId FROM task INNER JOIN project WHERE task.prjId=project.id AND task.state=".$state." AND project.id=".$each['id']." ORDER BY task.endTime"; 
+            $tasks=sql_str($sql); 
+            if(!empty($tasks)){ 
+                $count=0; 
+                foreach($tasks as $each){ 
+                    if($each["userId"]!=NULL){
+                        $sql="SELECT name FROM user WHERE id=".$each["userId"];
+                        $hasId=strpos($each["userId"],"*"); 
+                        if(!$hasId){
+                            $getName=sql_str($sql); 
+                            $tasks[$count]["receiver"]=$getName[0]["name"];
+                        }
+                        else{
+                            $tasks[$count]["receiver"]="无人接包";
+                        }
+                    }
+    
+                    else{
+                        $tasks[$count]["receiver"]="";
+                    }
+                    $count++; 
+                } 
+                // array_push($taskArr,$tasks); 
+                 
+                $taskArr = array_merge($taskArr,$tasks);  
+            } 
+        } 
+    }
+    else{
+        $projectId=$prjId;
+        $sql="SELECT task.taskName,task.id,task.startTime,task.endTime,task.securityLevel,task.state,project.prjName,task.userId FROM task INNER JOIN project WHERE task.prjId=project.id AND task.state=".$state." AND project.id=".$projectId." ORDER BY task.endTime";
         $tasks=sql_str($sql); 
         if(!empty($tasks)){ 
             $count=0; 
             foreach($tasks as $each){ 
                 if($each["userId"]!=NULL){
-                    $sql="SELECT name FROM user WHERE id=".$each["userId"]; 
-                    $getName=sql_str($sql); 
-                    $tasks[$count]["receiver"]=$getName[0]["name"];
+                    $sql="SELECT name FROM user WHERE id=".$each["userId"];  
+                    if(!$hasId){
+                        $getName=sql_str($sql);
+                        $tasks[$count]["receiver"]=$getName[0]["name"];
+                    }
+                    else{
+                        $tasks[$count]["receiver"]="无人接包";
+                    }
                 }
 
                 else{
@@ -33,10 +70,10 @@ if(!empty($user)){
                 $count++; 
             } 
             // array_push($taskArr,$tasks); 
-             
+                
             $taskArr = array_merge($taskArr,$tasks);  
         } 
-    } 
+    }
      
     $code=1; 
 }  
